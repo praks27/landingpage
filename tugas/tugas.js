6b1659ebdba87111609844988ee1ef477fe44f38
praks27 <gamingmaster456789@gmail.com>
2021-12-15 09:16:12 +0700

    tugas

--- tugas/tugas.js
+++ tugas/tugas.js
@@ -17,14 +17,21 @@ document.write("</br>selamat anda " + nilairata);
 
 document.write("<br><br>2.array harga");
 let diskon;
+let total = 0;
 let harga =[20000, 40000 , 65000 ,80000];
+let ttl_diskon = 0;
 for(let i = 0; i < harga.length; i++){
   document.write("<br>data barang-" + i + " harga = " + harga[i]);
   if(harga[i] >= 50000){
     diskon = 0.05 * harga[i];
     document.write("<br>diskon 5% = " + diskon);
+    ttl_diskon = ttl_diskon + diskon; 
   }
+  // total += harga;
+  total = total + harga[i];
 }
+document.write("harga total diskon : " + ttl_diskon +"<br>");
+document.write("harga total : " + total + "<br>");
 document.write("<br><br>3.OSPEK");
 let daftar;
 daftar = "sudah"
